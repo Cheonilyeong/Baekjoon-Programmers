9ea954f8e7673c873e9459c2d7c39af322ce1571
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-19 15:50:23 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main(void) {
+    cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+}
