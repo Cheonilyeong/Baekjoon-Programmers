c9998f36b34d4c06f8dcaa6c512ba3ba21281bb3
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-20 11:08:17 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    if(my_string.size() < is_suffix.size()) return 0;
+    return (my_string.substr(my_string.size()-is_suffix.size()) == is_suffix);
+}
