6483ccf9585bc153d8c421ee96082f47be903df1
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-20 11:39:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c;
+
+int main(void) {
+    cin >> a >> b >> c;
+    if(a == b && b == c) cout << 10000 + a * 1000;
+    else if(a == b) cout << 1000 + a * 100;
+    else if(a == c) cout << 1000 + a * 100;
+    else if(b == c) cout << 1000 + b * 100;
+    else if(a > b && a > c) cout << a * 100;
+    else if(b > a && b > c) cout << b * 100;
+    else if(c > a && c > b) cout << c * 100;
+}
