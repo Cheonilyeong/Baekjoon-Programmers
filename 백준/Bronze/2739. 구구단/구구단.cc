462f15f043c2ff3d9b0094e8f5ea4adf2f414d32
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-21 16:04:40 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(void) {
+    cin >> n;
+    for(int i = 1; i < 10; i++) {
+        cout << n << " * " << i << " = " << n * i << '\n';
+    }
+}
