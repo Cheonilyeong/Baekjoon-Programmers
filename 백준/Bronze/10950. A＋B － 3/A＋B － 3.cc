1bd01e9e929cc8549218c66c278e6de1c5b23308
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-21 16:06:28 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, t;
+
+int main(void) {
+    cin >> t;
+    for(int i = 0; i<t; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';  
+    }
+}
