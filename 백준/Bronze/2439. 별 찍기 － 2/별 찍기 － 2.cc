b132cdf88f20ea29f8ba07b926b887d437889d93
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-21 16:57:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < (n - i); j++)    cout << ' ';
+        for(int j = 0; j < i; j++)  cout << '*';
+        cout << '\n';
+    }
+}
