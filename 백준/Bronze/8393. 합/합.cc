db5b9c6c060d26a60a297b5629838f0d982a6432
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-21 16:10:19 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, sum;
+
+int main(void) {
+    cin >> n;
+    cout << n * (n + 1) / 2;
+    /*
+    for(int i = 0; i < n + 1; i++)
+        sum += i;
+    cout << sum << '\n';
+    */
+}
