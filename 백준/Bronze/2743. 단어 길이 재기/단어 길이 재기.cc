21794e7e78ea2ce0e9df93f5d5f007dc3d5cf7ae
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-22 11:01:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> s;
+    cout << s.size();
+}
