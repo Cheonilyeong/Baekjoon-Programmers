d24ad5c37115c9789675c3e777218f1539ad0c93
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-22 11:03:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char c;
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> c;
+    cout << (int)c;
+}
