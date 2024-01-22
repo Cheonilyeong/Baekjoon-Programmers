83b39c627cf22099a0df18da35394fccb04209c4
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-22 10:21:41 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.41 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> intervals) {
+    vector<int> answer;
+    for(vector<int> v : intervals) {
+        for(int i = v[0]; i < v[1]+1; i++) {
+            answer.push_back(arr[i]);
+        }
+    }
+    return answer;
+}
