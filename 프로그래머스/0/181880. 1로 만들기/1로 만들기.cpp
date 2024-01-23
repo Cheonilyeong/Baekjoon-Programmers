cbb5a7bbf94253a3db98c08202487ab9c4e39545
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-23 11:04:55 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    for(int i : num_list) {
+        
+        while(i != 1) {
+            
+            if(i & 1) i = (i - 1) / 2;
+            else i /= 2;
+            answer++;
+            
+        }
+    }
+    return answer;
+}
