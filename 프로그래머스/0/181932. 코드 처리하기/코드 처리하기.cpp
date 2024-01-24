b5bf074ebcd7af4219c697c16f3eaccc2860bb6e
Cheonilyeong <147039081+Cheonilyeong@users.noreply.github.com>
2024-01-24 11:43:40 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string code) {
+    string ret = "";
+    int mode = 0;
+    
+    for(int idx = 0; idx < code.size(); idx++) 
+    {
+        if(mode == 0) 
+        {
+            if(code[idx] != '1' && (idx & 1) == 0) 
+            {
+                ret += code[idx];
+            }
+            else if(code[idx] == '1') 
+            {
+                mode = 1;
+            }
+        }
+        else 
+        {
+            if(code[idx] != '1' && (idx & 1)) 
+            {
+                ret += code[idx];
+            }
+            else if(code[idx] == '1') 
+            {
+                mode = 0;
+            }
+        }
+    }
+    if(!ret.size()) ret = "EMPTY";
+    return ret;
+}
