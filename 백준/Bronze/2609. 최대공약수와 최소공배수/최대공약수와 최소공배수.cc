1bf736cba5f61a09930343a51973728af559caf1
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-04-09 21:34:29 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int gcd(int a, int b) {
+    if(a == 0) return b;
+    return gcd(b%a, a);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a,b);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> a >> b;
+    if(a > b) swap(a,b);
+
+    cout << gcd(a,b) << '\n';
+    cout << lcm(a,b) << '\n';
+}
