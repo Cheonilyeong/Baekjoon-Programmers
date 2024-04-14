3259186c16bf27eab3e4d63fc97555ad612ee7f2
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-04-15 00:47:33 +0900

    [Gold V] Title: 리모컨, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, num, checked[15], visited[1111111];
+int N, M, num, cnt=INT_MAX, broken[15];
 queue<int> q;
 
+bool check(string s) {
+    for(char c : s) {
+        if(broken[c-'0']) return false;
+    }
+    return true;
+}
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
@@ -11,44 +17,20 @@ int main(void) {
 
     for(int i = 0; i < M; i++) {
         cin >> num;
-        checked[num] = 1;
+        broken[num] = 1;
     }
 
-    visited[100] = 1;
-    visited[99] = 2;
-    visited[101] = 2;
-    q.push(99);
-    q.push(101);
+    // 채널 100에서 이동
+    cnt = abs(100-N);
 
+    // 채널 이동 후 + or -
     for(int i = 0; i <= 1111111; i++) {
         string s = to_string(i);
-        bool flag = true;
-        for(char c : s) {
-            if(checked[c-'0']) {
-                flag = false;
-                break;
-            }
+
+        if(check(s)) {
+            cnt = min(cnt, (int)s.size()+abs(N-i));
         }
-        if(!flag || visited[i]) continue;
-        visited[i] = s.size()+1;
-        q.push(i);
     }
-
-    while(q.size()) {
-        int now = q.front(); q.pop();
         
-        int next;
-        if(now < N) next = now+1;
-        if(now > N) next = now-1;
-
-        if(next < 0 || next >= 1111111) continue;
-        
-        if(visited[next] == 0 || visited[next] > visited[now]+1) {
-            visited[next] = visited[now] + 1;
-            q.push(next);
-        }
-
-    }
-
-    cout << visited[N] - 1;
+    cout << cnt;
 }
