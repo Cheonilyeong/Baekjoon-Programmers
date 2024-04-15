773e6b3a08355e7b2c5f9e14bdc2faa770d98a0a
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-04-15 10:42:04 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -1,53 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, a, b, visited[104], cnt[104][104];
+int N, M, a, b, dist[104][104];
 int mn=INT_MAX, ret=INT_MAX;
+const int INF = 1e9;
 vector<int> v[104];
-
-void bfs(int num) {
-    queue<int> q;
-
-    visited[num] = 1;
-    q.push(num);
-
-    while(q.size()) {
-        int now = q.front(); q.pop();
-
-        for(int next : v[now]) {
-            if(visited[next]) continue;
-            visited[next] = visited[now] + 1;
-            cnt[num][next] = visited[next];
-            q.push(next);
-        }
-    }
-
-    memset(visited,0,sizeof(visited));
-}
 
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    fill(&dist[0][0], &dist[103][103], INF);
 
     cin >> N >> M;
 
     for(int i = 0; i < M; i++) {
         cin >> a >> b;
-        v[a-1].push_back(b-1);
-        v[b-1].push_back(a-1);
+        dist[a-1][b-1] = 1;
+        dist[b-1][a-1] = 1;
     }
 
-    for(int i = 0; i < N; i++) {
-        bfs(i);
+    for(int k = 0; k < N; k++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
     }
 
     for(int i = 0; i < N; i++) {
         int sum = 0;
         for(int j = 0; j < N; j++) {
             if(i == j) continue;
-            sum += cnt[i][j]-1;
+            sum += dist[i][j];
         }
-        if(mn == sum) ret=min(ret,i);
-        if(mn > sum) mn=sum, ret=i;
+        if(mn>sum) mn=sum, ret=i;
     }
 
     cout << ret+1;
