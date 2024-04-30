7bd9c06886c6df9f31c64743be487dad983e9192
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-04-30 15:31:10 +0900

    [Bronze V] Title: ??!, Time: 88 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`));
+
+    var str = br.readLine();
+
+    println(str+"??!");
+}
