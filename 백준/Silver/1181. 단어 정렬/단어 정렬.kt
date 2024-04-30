768819c91dd58b24ed8b26ac532bd3dda3224196
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-04-30 19:00:24 +0900

    [Silver V] Title: 단어 정렬, Time: 536 ms, Memory: 36916 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val br = BufferedReader(InputStreamReader(System.`in`));
+    val bw = BufferedWriter(OutputStreamWriter(System.out));
+
+    val N = br.readLine().toInt();
+
+    val arr = ArrayList<String>();
+    val set = HashSet<String>();
+
+    (1..N).forEach() {
+        val word = br.readLine();
+        if(set.contains(word)) return@forEach;
+        set.add(word);
+        arr.add(word);
+    }
+
+    arr.sortWith(compareBy ({it.length}, {it}));
+
+    arr.forEach() {
+        bw.write(it+"\n")
+    };
+
+    br.close();
+    bw.flush();
+    bw.close();
+}
