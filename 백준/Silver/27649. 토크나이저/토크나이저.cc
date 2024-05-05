f2166f194403a248fe252faaafbb5228ccddcf19
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-05-05 12:13:16 +0900

    [Silver III] Title: 토크나이저, Time: 24 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Silver/27649. 토크나이저/토크나이저.cc
+++ 백준/Silver/27649. 토크나이저/토크나이저.cc
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s, t, ret;
+string s, ret;
 char pre;
-vector<string> v;
-
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     getline(cin, s);
 
-    for(int idx = 0; idx < s.size(); idx++) {
-        char c = s[idx];
+    for(char c : s) {
         // 구분자면 앞 뒤에 공백 추가
-        if(c=='<'||c=='>'||c=='('||c==')'||c==' ') {
-            if(t.size())v.push_back(t); t="";
-            if(c!=' ')v.push_back(string(1,c));
+        if(c=='<'||c=='>'||c=='('||c==')') {
+            if(ret.size() && ret.back()==' ') ret+=c, ret+=' ';
+            else if(ret.size() && ret.back()!=' ') ret+=' ', ret+=c, ret+=' ';
+            else if(ret.empty()) ret+=c, ret+=' '; 
         }
         // 구분자(& or |)면 앞 뒤에 공백 추가하고 &는 2번
         else if(c=='&'||c=='|') {
-            if(pre == c) {pre = '\0'; continue;}
-            if(t.size())v.push_back(t); t="";
-            v.push_back(string(1,c)+c);
+            if(pre == c) {pre='\0';continue;}
+            if(ret.size() && ret.back()==' ') ret+=c, ret+=c, ret+=' ';
+            else if(ret.size() && ret.back()!=' ') ret+=' ', ret+=c, ret+=c, ret+=' ';
+            else if(ret.empty()) ret+=c, ret+=c, ret+=' '; 
+        }
+        // 공백이면
+        else if(c == ' ') {
+            if(ret.size() && ret.back()!=' ') ret+=c;
         }
         // 나머지(ex.alpha)
-        else t += c;
+        else ret+=c;
 
         pre = c;
     }
-    if(t.size())v.push_back(t);
 
-    for(int i = 0; i < v.size(); i++) {
-        cout << v[i];
-        if(i != v.size()-1) cout << ' ';
-    }
+    if(ret.back()==' ') ret.erase(ret.size()-1);
+    cout << ret;
 }
