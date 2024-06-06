abcb9ca4e8b172eca6f10a862ddd26e665a4fcd5
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-06-06 10:51:09 +0900

    [Silver II] Title: 이동3, Time: 832 ms, Memory: 81140 KB -BaekjoonHub

---
+++ 백준/Silver/12993. 이동3/이동3.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll x, y;
+set<pair<ll,ll>> visited;
+
+ll pow(ll a, ll b) {
+    ll result = 1;
+    while(b--) {
+        result *= a;
+    }
+
+    return result;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> x >> y;
+
+    queue<tuple<ll,ll,ll>> q;
+    q.push({0,0,0});
+    visited.insert({0,0});
+
+    bool flag = false;
+    while(q.size()) {
+        auto now = q.front(); q.pop();
+        ll nowX = get<0>(now);
+        ll nowY = get<1>(now);
+        ll k = get<2>(now);
+        
+        if(nowX == x && nowY == y) {
+            flag = true;
+            break;
+        }
+
+        // x좌표 증가
+        ll nextX = nowX + pow(3,k);
+        ll nextY = nowY;
+        if(nextX <= x && nextY <= y) {
+            if(visited.find({nextX, nextY}) == visited.end()) {
+                visited.insert({nextX, nextY});
+                q.push({nextX, nextY, k+1});
+            }
+        }
+
+        // y좌표 증가
+        nextX = nowX;
+        nextY = nowY + pow(3,k);
+        if(nextX <= x && nextY <= y) {
+            if(visited.find({nextX, nextY}) == visited.end()) {
+                visited.insert({nextX, nextY});
+                q.push({nextX, nextY, k+1});
+            }
+        }
+    }
+
+    if(flag) cout << 1;
+    else cout << 0;
+}
