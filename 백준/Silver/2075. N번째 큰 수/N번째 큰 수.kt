1dc76bfc1ce8de5b218802444ebf418560dd72dc
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-06-25 10:04:16 +0900

    [Silver II] Title: N번째 큰 수, Time: 1160 ms, Memory: 334988 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.kt
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.kt
@@ -2,6 +2,8 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayList
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
@@ -10,15 +12,22 @@ fun main() {
     val N = br.readLine().toInt()
 
     val list = ArrayList<Int>()
+    val pq = PriorityQueue<Int>(compareByDescending { it })
     for(i in 0 until N) {
         val num = br.readLine().split(" ").map{ it.toInt() }
         for(j in 0 until N) {
-            list.add(num[j])
+            pq.offer(num[j])
         }
     }
-    list.sort()
+//    list.sort()
+//    list.sortBy { it }
+//    list.sortWith(compareBy{it})
 
-    println("${list[N*N-N]}")
+
+    for(i in 0 until N-1) {
+        pq.poll()
+    }
+    println("${pq.poll()}")
 
     br.close()
     bw.close()
