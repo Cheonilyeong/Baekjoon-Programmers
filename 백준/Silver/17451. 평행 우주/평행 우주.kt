1d854df646d3bdf5437e4b8853d4cf6d0f91bf8f
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-07-10 23:48:10 +0900

    [Silver III] Title: 평행 우주, Time: 432 ms, Memory: 78428 KB -BaekjoonHub

---
+++ 백준/Silver/17451. 평행 우주/평행 우주.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val N = br.readLine().toInt()
+    val arr = br.readLine().trim().split(" ").map { it.toLong() }.reversed()
+
+    var speed = 0L
+    arr.forEach {
+        if(speed > it) {
+            speed = findSpeed(it, speed)
+        }
+        else {
+            speed = it
+        }
+    }
+
+    println("$speed")
+
+    br.close()
+    bw.close()
+}
+
+fun findSpeed(num: Long, min: Long) = (min + num - 1) / num * num
